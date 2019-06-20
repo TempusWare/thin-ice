bb1a3bcf876c5885895cddda173a93ad93f54c58
Tempus <tempusware@gmail.com>
2019-06-20 17:32:12 +1000

    Levels and Graphics
    Added 4 more levels and a graphics mode

--- engine.js
+++ engine.js
@@ -4,160 +4,293 @@ var levelRows = 19;
 var levelCols = 15;
 var tileSize = 24;
 
-var level1 = [
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1],
-  [1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,2,1,1,1],
-  [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [15,11,3,11]
-];
-
-var level2 = [
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,2,2,2,1,1,1],
-  [1,1,1,1,1,2,2,2,2,2,2,2,1,2,0,2,1,1,1],
-  [1,1,1,1,1,2,0,0,0,0,0,2,1,2,0,2,1,1,1],
-  [1,2,2,2,2,2,0,2,2,2,0,2,2,2,0,2,1,1,1],
-  [1,2,0,0,0,0,0,2,1,2,0,0,0,0,0,2,1,1,1],
-  [1,2,2,2,2,2,2,2,1,2,2,2,2,2,2,2,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
-  [3,11,15,8]
-];
-
-var level = level2;
-var playerRow = level[15][0] - 1;
-var playerCol = level[15][1] - 1;
-var goalRow = level[15][2] - 1;
-var goalCol = level[15][3] - 1;
-
 canvas.height = tileSize * levelCols;
 canvas.width = tileSize * levelRows;
 
+var idTile = 0;
+var idTileImg = new Image();
+idTileImg.src = "sprites/" + idTile + ".png";
+var idIce = 1;
+var idIceImg = new Image();
+idIceImg.src = "sprites/" + idIce + ".png";
+var idCoin = 2;
+var idCoinImg = new Image();
+idCoinImg.src = "sprites/" + idCoin + ".png";
+var idGoal = 9;
+var idGoalImg = new Image();
+idGoalImg.src = "sprites/" + idGoal + ".png";
+var idGoalImg = new Image();
+idGoalImg.src = "sprites/" + idGoal + ".png";
+var idPlayer = 10;
+var idPlayerImg = new Image();
+idPlayerImg.src = "sprites/" + idPlayer + ".png";
+var idOutside = 11;
+var idOutsideImg = new Image();
+idOutsideImg.src = "sprites/" + idOutside + ".png";
+var idWall = 12;
+var idWallImg = new Image();
+idWallImg.src = "sprites/" + idWall + ".png";
+var idWater = 13;
+var idWaterImg = new Image();
+idWaterImg.src = "sprites/" + idWater + ".png";
+
+var level1 = [
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,11,11,11],
+  [11,12,9,0,0,0,0,0,0,0,0,0,0,0,10,12,11,11,11],
+  [11,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+];
+var level2 = [
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,12,12,12,11,11,11],
+  [11,11,11,11,11,12,12,12,12,12,12,12,11,12,9,12,11,11,11],
+  [11,11,11,11,11,12,0,0,0,0,0,12,11,12,0,12,11,11,11],
+  [11,12,12,12,12,12,0,12,12,12,0,12,12,12,0,12,11,11,11],
+  [11,12,10,0,0,0,0,12,11,12,0,0,0,0,0,12,11,11,11],
+  [11,12,12,12,12,12,12,12,11,12,12,12,12,12,12,12,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+];
+var level3 = [
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,12,12,12,11,11,11,11,11,11,11,11,12,12,12,11,11,11],
+  [11,11,12,9,12,11,11,11,11,11,11,11,11,12,10,12,11,11,11],
+  [11,11,12,0,12,12,12,12,12,11,11,11,11,12,0,12,11,11,11],
+  [11,11,12,0,12,12,0,0,12,12,12,12,12,12,0,12,11,11,11],
+  [11,11,12,0,0,0,0,0,0,0,0,0,0,0,0,12,11,11,11],
+  [11,11,12,0,0,12,12,12,12,0,0,12,12,0,0,12,11,11,11],
+  [11,11,12,12,12,12,11,11,12,12,12,12,12,12,12,12,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+];
+var level4 = [
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,12,12,12,12,12,11,11,11,11,11,11,12,12,12,12,12,11,11],
+  [11,12,0,0,0,12,12,12,12,12,12,12,12,0,0,0,12,11,11],
+  [11,12,0,0,0,12,12,0,0,0,0,12,12,0,0,0,12,11,11],
+  [11,12,12,0,0,0,0,0,0,0,0,0,0,0,0,12,12,11,11],
+  [11,11,12,0,12,12,12,12,0,0,12,12,12,12,0,12,11,11,11],
+  [11,11,12,0,12,11,12,0,0,0,0,12,11,12,0,12,11,11,11],
+  [11,11,12,10,12,11,12,2,0,0,0,12,11,12,9,12,11,11,11],
+  [11,11,12,12,12,11,12,12,12,12,12,12,11,12,12,12,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+];
+var level8 = [
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11,11],
+  [12,12,12,12,12,11,11,11,12,12,12,12,12,11,11,11,11,11,11],
+  [12,0,0,0,12,12,12,12,12,0,0,0,12,11,11,11,11,11,11],
+  [12,0,12,0,12,12,10,12,12,0,12,0,12,11,11,11,11,11,11],
+  [12,0,0,1,0,0,1,0,0,1,0,0,12,12,12,12,11,11,11],
+  [12,12,12,0,12,12,0,12,12,0,12,12,12,12,9,12,11,11,11],
+  [11,12,12,0,12,12,0,12,12,0,12,12,12,12,0,12,11,11,11],
+  [11,12,0,0,0,0,1,0,0,1,0,0,12,0,0,12,12,11,11],
+  [11,12,2,0,0,12,0,12,12,0,12,0,12,0,0,0,12,11,11],
+  [11,12,0,0,0,0,0,12,12,0,0,0,12,12,0,0,12,11,11],
+  [11,12,0,0,12,12,12,12,12,12,12,12,12,12,0,0,12,11,11],
+  [11,12,12,0,0,0,0,0,0,0,0,0,0,0,0,0,12,11,11],
+  [11,11,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,11,11],
+];
+var level = [];
+// Level selector
+function selectLevel(number) {
+  switch (number) {
+    case "1":
+      level = level1;
+      break;
+    case "2":
+      level = level2;
+      break;
+    case "3":
+      level = level3;
+      break;
+    case "4":
+      level = level4;
+      break;
+    case "8":
+      level = level8;
+      break;
+  };
+  startGame()
+};
+
+var graphicsImg;
+
+function startGame() {
+// Find player and goal positions in level
+for (var c = 0; c < levelCols; c++) {
+  console.log(c + " " + level[c])
+  if (level[c].includes(idPlayer)) {
+    var playerCol = c;
+    var playerRow = level[c].indexOf(idPlayer)
+  };
+  if (level[c].includes(idGoal)) {
+    var goalCol = c;
+    var goalRow = level[c].indexOf(idGoal)
+  }
+};
+console.log("Player Row: " + playerRow + " | Player Column: " + playerCol + "\nGoal Row: " + goalRow + " | Goal Column: " + goalCol)
 renderLevel();
-
-var leftPressed;
-var rightPressed;
-var upPressed;
-var downPressed;
-
-x = 100;
-
 function gameLoop() {
-  if (leftPressed) {
-    if (level[playerCol][playerRow - 1] != 1 && level[playerCol][playerRow - 1] != 2 && level[playerCol][playerRow - 1] != 3) {
-      level[playerCol][playerRow] = 3;
+  switch (keypress) {
+    case "left":
+      if (level[playerCol][playerRow - 1] > idPlayer) {break};
+      if (level[playerCol][playerRow] == idIce) {level[playerCol][playerRow] = idTile}
+      else {level[playerCol][playerRow] = idWater};
       playerRow--;
-    };
-  };
-  if (rightPressed) {
-    if (level[playerCol][playerRow + 1] != 1 && level[playerCol][playerRow + 1] != 2 && level[playerCol][playerRow + 1] != 3) {
-      level[playerCol][playerRow] = 3;
+      break
+    case "right":
+      if (level[playerCol][playerRow + 1] > idPlayer) {break};
+      if (level[playerCol][playerRow] == idIce) {level[playerCol][playerRow] = idTile}
+      else {level[playerCol][playerRow] = idWater};
       playerRow++
-    };
-  };
-  if (upPressed) {
-    if (level[playerCol - 1][playerRow] != 1 && level[playerCol - 1][playerRow] != 2 && level[playerCol - 1][playerRow] != 3) {
-      level[playerCol][playerRow] = 3;
-      playerCol--
-    };
-  };
-  if (downPressed) {
-    if (level[playerCol + 1][playerRow] != 1 && level[playerCol + 1][playerRow] != 2 && level[playerCol + 1][playerRow] != 3) {
-      level[playerCol][playerRow] = 3;
-      playerCol++
-    };
+      break
+    case "up":
+      if (level[playerCol - 1][playerRow] > idPlayer) {break};
+      if (level[playerCol][playerRow] == idIce) {level[playerCol][playerRow] = idTile}
+      else {level[playerCol][playerRow] = idWater};
+      playerCol--;
+      break
+    case "down":
+      if (level[playerCol + 1][playerRow] > idPlayer) {break};
+      if (level[playerCol][playerRow] == idIce) {level[playerCol][playerRow] = idTile}
+      else {level[playerCol][playerRow] = idWater};
+      playerCol++;
+      break
   };
 
-  renderLevel();
+  /*if (level[playerCol][playerRow + 1] != 0 && level[playerCol][playerRow - 1] != 0 && level[playerCol + 1][playerRow] != 0 && level[playerCol - 1][playerRow] != 0) {
+    level[playerCol][playerRow] = 3;
+    playerCol = 0;
+    playerRow = 0;
+    renderLevel();
+    return;
+  };*/
+
+  updateGame();
 
   setTimeout(gameLoop, 100)
 };
 gameLoop();
-
+function updateGame() {
+  if (level[playerCol][playerRow] == idCoin) {
+    level[playerCol][playerRow] = idPlayer;
+    console.log("COIN BAG collected")
+  };
+  if (playerRow == goalRow && playerCol == goalCol) {
+    //goalRow = 0;
+    //goalCol = 0;
+    console.log("You win!");
+    var allMelted = true;
+    for (var c = 0; c < levelCols; c++) {
+      if (level[c].includes(idTile)) {
+        allMelted = false;
+      }
+    };
+    if (allMelted) {console.log("All tiles have been melted!")}
+  };
+  if (level[playerCol + 1][playerRow] > 10 && level[playerCol - 1][playerRow] > 10 && level[playerCol][playerRow + 1] > 10 && level[playerCol][playerRow - 1] > 10) {
+    level[playerCol][playerRow] = idWater;
+    console.log("Game over")
+  }
+  renderLevel()
+}
 function renderLevel() {
 
   context.clearRect(0, 0, canvas.width, canvas.height);
   for (var c = 0; c < levelCols; c++) {
     for (var r = 0; r < levelRows; r++) {
       switch (level[c][r]) {
-        case 0:
+        case idTile: case idPlayer:
           context.fillStyle = "white";
-          context.fillRect(r * tileSize, c * tileSize, tileSize, tileSize)
+          context.fillRect(r * tileSize, c * tileSize, tileSize, tileSize);
+          if (graphicsImg) {context.drawImage(idTileImg, r * tileSize, c * tileSize)};
           break;
-        case 1:
+        case idCoin:
+          context.fillStyle = "orange";
+          context.fillRect(r * tileSize, c * tileSize, tileSize, tileSize)
+          if (graphicsImg) {context.drawImage(idCoinImg, r * tileSize, c * tileSize)};
+          break;
+        case idOutside:
           context.fillStyle = "#9BCDFF";
           context.fillRect(r * tileSize, c * tileSize, tileSize, tileSize)
+          if (graphicsImg) {context.drawImage(idOutsideImg, r * tileSize, c * tileSize)};
           break;
-        case 2:
+        case idIce:
+          context.fillStyle = "aqua";
+          context.fillRect(r * tileSize, c * tileSize, tileSize, tileSize)
+          if (graphicsImg) {context.drawImage(idIceImg, r * tileSize, c * tileSize)};
+          break;
+        case idWall:
           context.fillStyle = "#0066CC";
           context.fillRect(r * tileSize, c * tileSize, tileSize, tileSize)
+          if (graphicsImg) {context.drawImage(idWallImg, r * tileSize, c * tileSize)};
           break;
-        case 3:
+        case idWater:
           context.fillStyle = "darkblue";
           context.fillRect(r * tileSize, c * tileSize, tileSize, tileSize)
+          if (graphicsImg) {context.drawImage(idWaterImg, r * tileSize, c * tileSize)};
           break;
       }
     }
   };
   context.fillStyle = "red";
-  context.fillRect((goalRow) * tileSize, (goalCol) * tileSize, tileSize, tileSize);
-  context.fillStyle = "grey";
-  context.fillRect((playerRow) * tileSize, (playerCol) * tileSize, tileSize, tileSize);
-  if (playerRow == goalRow && playerCol == goalCol) {console.log("You win!")}
+  context.fillRect(goalRow * tileSize, goalCol * tileSize, tileSize, tileSize);
+  if (graphicsImg) {context.drawImage(idGoalImg, goalRow * tileSize, goalCol * tileSize)};
+  //if (playerRow == 0) {console.log("Game over!"); return};
+  if (level[playerCol][playerRow] <= idPlayer) {
+    context.fillStyle = "grey";
+    context.fillRect(playerRow * tileSize, playerCol * tileSize, tileSize, tileSize);
+    if (graphicsImg) {context.drawImage(idPlayerImg, playerRow * tileSize, playerCol * tileSize)}
+  }
+  //if (playerRow == goalRow && playerCol == goalCol) {console.log("You win!")}
 };
-
+var keypress;
 document.addEventListener("keydown", function(e) {
-  //console.log(e.keyCode);
   switch (e.keyCode) {
     case 65:
-      leftPressed = true;
+      keypress = "left";
       break;
 		case 68:
-      rightPressed = true;
+      keypress = "right";
 			break;
 		case 87:
-      upPressed = true;
+      keypress = "up";
 			break;
 		case 83:
-      downPressed = true;
+      keypress = "down";
 			break;
   };
-  //console.log("pr: " + playerRow + " pc: " + playerCol)
 });
-
 document.addEventListener("keyup", function(e) {
-  //console.log(e.keyCode);
-  switch (e.keyCode) {
-    case 65:
-      leftPressed = false;
-      break;
-		case 68:
-      rightPressed = false;
-			break;
-		case 87:
-      upPressed = false;
-			break;
-		case 83:
-      downPressed = false;
-			break;
-  };
-  //console.log("pr: " + playerRow + " pc: " + playerCol)
+  keypress = false;
 });
+}
